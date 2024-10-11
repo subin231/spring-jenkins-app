8d5d50b5c97449ecf443f1226e433a528e2d5b46
subin231 <subinasa231@gmail.com>
2024-10-11 12:02:45 +0900

    first commit

---
+++ src/main/groovy/kr/subin/SpringJenkinsAppApplication.groovy
@@ -0,0 +1,13 @@
+package kr.subin
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringJenkinsAppApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringJenkinsAppApplication, args)
+    }
+
+}
